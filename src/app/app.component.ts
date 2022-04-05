3ff9b656ebd0ca6c3105c46c9ee58bd4ff519ca3
sheldon <simba@codevirtus.com>
2022-04-05 13:00:12 +0200

    working example done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as Stomp from 'stompjs';
+
+// import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'websocket';
+  title = 'app';
+
+  greetings: string[] = [];
+  showConversation: boolean = false;
+  ws: any;
+  name!: string;
+  disabled!: boolean;
+
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    let socket = new WebSocket("ws://localhost:8069/opn/v1/notifications/my-notifications");
+    this.ws = Stomp.over(socket);
+    let that = this;
+    this.ws.connect({token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY0OTI0MjMyNiwiaWF0IjoxNjQ5MTU1OTI2fQ.rwkE8b6KkukNeSbenJ1geAlG4jx4pvt8Xp3CG5eUl7ie0LuAdPK5mrTSRRmB_jGtF3MowjBPVkYdY1KOTdvmew"}, function () {
+      that.ws.subscribe("/errors", function (message: { body: string; }) {
+        alert("Error " + message.body);
+      });
+      that.ws.subscribe("/user/topic/my-notifications", function (message: { body: string; }) {
+        console.log(message)
+        that.showGreeting(message.body);
+      });
+      that.disabled = true;
+    }, function (error: string) {
+      alert("STOMP error " + error);
+    });
+  }
+
+  disconnect() {
+    if (this.ws != null) {
+      this.ws.ws.close();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
+  }
+
+  sendName() {
+    let data = JSON.stringify({
+      'name': this.name
+    })
+    this.ws.send("/app/message", {}, data);
+  }
+
+  showGreeting(message: string) {
+    this.showConversation = true;
+    this.greetings.push(message)
+  }
+
+  setConnected(connected: boolean) {
+    this.disabled = connected;
+    this.showConversation = connected;
+    this.greetings = [];
+  }
 }
